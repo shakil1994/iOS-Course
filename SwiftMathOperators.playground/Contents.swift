5a6f433ca015631296b96511d62e1258cfb614c2
android_shakil <shakhawathossen53@gmail.com>
2021-04-07 17:31:10 +0600

    Added Swift Math Operators

---
+++ SwiftMathOperators.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var a = 2
+var b = 3
+var c = 4
+var d = 3.6
+
+
+print(a + b)
+print(a - c)
+print(c / a)
+print(b * c)
+
+pow(2, 3)
+
+sqrt(4)
+
+ceil(d)
+
+floor(d)
+
+a = a + 1
+a += 1 // a = a + 1
+a = a - 1
+a -= 1 // a = a - 1
