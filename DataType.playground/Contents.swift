a83c1901a55260b7fb486109adbfb01abad29203
android_shakil <shakhawathossen53@gmail.com>
2021-04-07 17:17:15 +0600

    Added Different types of DataType

---
+++ DataType.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var str: String = "Hello, playground"
+
+// Data types
+
+/*
+ String: Piece of text
+ Int: Whole numbers positive and negative
+ Float: Decemial numbers
+ Double: Large decimal numbers
+ Bool: True ot false
+ */
+
+var anInteger: Int = 12
+var aFloat: Float = 0.5
+var aDouble: Double = 0.6
+var aBool: Bool = true
+
+
+
