d5f1beeb5bc4997a63d2020bd8c419359082dddc
android_shakil <shakhawathossen53@gmail.com>
2021-04-07 17:02:12 +0600

    Added Variable var and let constant

---
+++ Variable.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+var str = "Hello, playground"
+
+123
+0.6
+"Hello"
+
+print(str)
+
+
+// var = variable key word
+// firstName = variable name
+var firstName = "Shakil"
+print(firstName)
+
+
+var stockPrice = 1234
+stockPrice = 50
+
+//You can't assign Tom to stockPrice because it's an integer you declared already
+//stockPrice = "Tom"
+print(stockPrice)
+
+let lastName = "Tanmoy"
+
+// Constant can't reassigned data
+//lastName = "Kundo"
+
+// Constants are useful to keep track of data that you need but you don't expect to change
+
